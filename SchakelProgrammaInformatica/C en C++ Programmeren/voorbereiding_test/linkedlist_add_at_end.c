2bd8653b88917ded0f333d17c8ce02d3ced011b9
Xavier Geerinck <xaviergeerinck@Xaviers-MacBook-Pro.local>
2014-11-09 17:42:20 +0100

    Add linkedlist manipulations

---
+++ SchakelProgrammaInformatica/C en C++ Programmeren/voorbereiding_test/linkedlist_add_at_end.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct Knoop Knoop;
+
+struct Knoop {
+    int waarde;
+    Knoop * next;
+};
+
+Knoop * maak_lijst();
+
+int main() {
+    Knoop *h;
+    Knoop *lijst_start = maak_lijst();
+
+    /* Print list */
+    h = lijst_start;
+
+    while (h) {
+        printf("Waarde: %d \n", h->waarde);
+
+        h = h->next;
+    }
+
+    /* Clear memory */
+    free(lijst_start);
+}
+
+Knoop * maak_lijst() {
+    Knoop *start = NULL;
+    Knoop *h;
+
+    int g;
+    scanf("%d", &g);
+
+    while (g != 0) {
+        if (g != 0) {
+            Knoop *n = malloc(sizeof(Knoop));
+            n->waarde = g;
+            n->next = NULL;
+
+            /* Voeg achteraan toe */
+            h = start;
+            while (h && h->next) {
+                h = h->next;
+            }
+
+            if (!start) {
+                start = n;
+            } else {
+                h->next = n;
+            }
+        }
+
+        scanf("%d", &g);
+    }
+
+    return start;
+}
